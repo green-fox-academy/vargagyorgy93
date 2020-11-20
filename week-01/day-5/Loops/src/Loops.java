98cbfc4e63131258f7f3e05b779b0657b7af5832
vargagyorgy93 <vargagyorgy93@gmail.com>
2020-11-20 15:33:47 +0100

    first week work

---
+++ week-01/day-5/Loops/src/Loops.java
@@ -0,0 +1,16 @@
+//gyakorlás, nem egy feladat
+
+public class Loops {
+    public static void main(String[] args) {
+        int a = 0;
+
+        while (a < 10) {
+            System.out.println(a);
+            a += 1;
+        }
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
