46640db1357149a6af0c9669d10385468c2cd3a0
vargagyorgy93 <vargagyorgy93@gmail.com>
2020-11-23 16:10:30 +0100

    secound week work

---
+++ week-02/day-01/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        greetFunction();
+    }
+
+    public static void greetFunction() {
+        System.out.println("Hello Green Fox!");
+    }
+}
+
