b8b4e8a46d2d497f95b1b6e1f810db33276241b5
vargagyorgy93 <vargagyorgy93@gmail.com>
2020-11-23 12:20:37 +0100

    secound week work

---
+++ week-02/day-01/Workshop/src/Workshop.java
@@ -0,0 +1,13 @@
+public class Workshop {
+    public static void main(String[] args) {
+        int[] myList = {1, 2, 3, 6};
+
+        System.out.println("The secound element is " + myList[1]);
+
+        for (int i = 0; i < myList.length; i++) {
+            System.out.println(myList[i] + " ");
+
+        }
+
+    }
+}
