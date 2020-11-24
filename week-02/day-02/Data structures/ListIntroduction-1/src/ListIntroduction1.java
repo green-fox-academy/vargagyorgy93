6e982ba0bc5326fed1c765992e674419f6b93bf0
vargagyorgy93 <vargagyorgy93@gmail.com>
2020-11-24 17:19:22 +0100

    secound week work

---
+++ week-02/day-02/Data structures/ListIntroduction-1/src/ListIntroduction1.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.io.*;
+import java.util.*;
+
+
+public class ListIntroduction1 {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        System.out.println(names.size());
+
+        names.add("William");
+        if (names.isEmpty()) {
+            System.out.println("Empty");
+        } else {
+            System.out.println("Not Empty");
+        }
+        names.add("John");
+        names.add("Amanda");
+        System.out.println(names.size());
+        System.out.println(names.get(2));
+
+        for (String elements : names) {
+            System.out.println(elements);
+        }
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println((i + 1) + ". " + names.get(i));
+        }
+
+        names.remove(1);
+
+//        for (String removed : names) {
+//            System.out.println(removed);
+//        }
+
+        Collections.reverse(names);
+        for (String reverse : names) {
+            System.out.println(reverse);
+        }
+
+        names.clear();
+
+    }
+}
