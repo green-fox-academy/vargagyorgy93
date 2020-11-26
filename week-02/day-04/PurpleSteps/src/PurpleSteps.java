65ead59a4905d61abd0a000f32fa8afe6e8676a9
vargagyorgy93 <vargagyorgy93@gmail.com>
2020-11-26 11:19:41 +0100

    secound week work

---
+++ week-02/day-04/PurpleSteps/src/PurpleSteps.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps {
+    public static void mainDraw(Graphics graphics) {
+
+
+        graphics.setColor(new Color(147, 112, 219));
+
+
+        int width = 10;
+        int height = 10;
+
+
+        for (int i = 0; i < 19; i++) {
+            graphics.fillRect(i * width, i * height, width, height);
+        }
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
---
+++ week-02/day-04/PurpleSteps3D/src/PurpleSteps3D.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps3D {
+    public static void mainDraw(Graphics graphics) {
+
+        graphics.setColor(new Color(147, 112, 219));
+
+
+        int size = 10;
+        int distance = 0;
+
+        graphics.fillRect(distance, distance, size, size);
+
+        for (int i = 0; i < 5; i++) {
+            distance = size + distance;
+            size *= 2;
+            graphics.fillRect(distance, distance, size, size);
+        }
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
