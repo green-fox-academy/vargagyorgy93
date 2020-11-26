1ae200c34597d903f52f28857c63aecb4934bea4
vargagyorgy93 <vargagyorgy93@gmail.com>
2020-11-26 09:39:09 +0100

    secound week work

---
+++ week-02/day-03/Checkerboard/src/Checkerboard.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Checkerboard {
+    public static void mainDraw(Graphics graphics) {
+        // Fill the canvas with a checkerboard pattern.
+
+
+        int x = -288;
+        int y = 0;
+        int width = 32;
+        int height = 32;
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+
+                graphics.fillRect(x + (j * width), y + (j * height), width, height);
+
+            }
+            x += (2 * width);
+
+        }
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
