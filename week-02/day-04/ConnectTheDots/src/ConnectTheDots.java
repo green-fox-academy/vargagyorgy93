20dc6a8069752dddb60070dbf474363860978e2a
vargagyorgy93 <vargagyorgy93@gmail.com>
2020-11-29 15:45:45 +0100

    secound week work

--- week-02/day-04/ConnectTheDots/src/ConnectTheDots.java
+++ week-02/day-04/ConnectTheDots/src/ConnectTheDots.java
@@ -12,23 +12,48 @@ public class ConnectTheDots {
         // An array of {x, y} points and graphics
         // and connects them with green lines.
         // Connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
-        // Connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
-        // {120, 100}, {85, 130}, {50, 100}}
+//         Connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+//         {120, 100}, {85, 130}, {50, 100}}
 
 
         graphics.setColor(Color.GREEN);
 
-        int[] xCoords = {10, 290, 290, 10};
-        int[] yCoords = {10, 10, 290, 290};
-        int lenght = 4;
-        int[] box = {10, 10, 290, 10, 290, 290, 10, 290};
 
-        int[] x2Coords = {50, 70, 80, 90, 100, 120, 85, 50,};
-        int[] y2Coords = {100, 70, 90, 90, 70, 100, 130, 100};
-        int lenght2 = 8;
+        int[][] box = {{10, 10}, {290, 10}, {290, 290}, {10, 290}};
 
-        graphics.drawPolygon(xCoords, yCoords, lenght);
-        graphics.drawPolygon(x2Coords, y2Coords, lenght2);
+        int[] x = new int[box.length];
+        int[] y = new int[box.length];
+        for (int i = 0; i < box.length; i++) {
+            x[i] = box[i][0];
+            y[i] = box[i][1];
+        }
+        graphics.drawPolygon(x, y, box.length);
+
+        int[][] fox = {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+                {120, 100}, {85, 130}, {50, 100}};
+
+        int[] x2 = new int[fox.length];
+        int[] y2 = new int[fox.length];
+        for (int i = 0; i < fox.length; i++) {
+            x2[i] = fox[i][0];
+            y2[i] = fox[i][1];
+        }
+        graphics.drawPolygon(x2, y2, fox.length);
+
+
+//        int[] xCoords = {10, 290, 290, 10};
+//        int[] yCoords = {10, 10, 290, 290};
+//        int lenght = 4;
+//        int[] box = {10, 10, 290, 10, 290, 290, 10, 290};
+//
+//        int[] x2Coords = {50, 70, 80, 90, 100, 120, 85, 50,};
+//        int[] y2Coords = {100, 70, 90, 90, 70, 100, 130, 100};
+//        int lenght2 = 8;
+//
+//        graphics.drawPolygon(xCoords, yCoords, lenght);
+//        graphics.drawPolygon(x2Coords, y2Coords, lenght2);
+
+
     }
 
 
