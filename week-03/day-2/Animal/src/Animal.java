788f95c3d362a0ad123755ac4aa7b9677bcbd791
vargagyorgy93 <vargagyorgy93@gmail.com>
2020-12-02 13:59:44 +0100

    third week work

---
+++ week-03/day-2/Animal/src/Animal.java
@@ -0,0 +1,24 @@
+public class Animal {
+    int hunger = 50;
+    int thirst = 50;
+
+
+    public void eat() {
+        hunger -= 1;
+    }
+
+    public void drink() {
+        thirst -= 1;
+    }
+
+    public void play() {
+        hunger -= 1;
+        thirst -= 1;
+    }
+
+    public static void main(String[] args) {
+        
+
+    }
+
+}
