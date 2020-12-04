19158b0038c00637df1104a3931ece88afcaab4c
vargagyorgy93 <vargagyorgy93@gmail.com>
2020-12-04 15:20:41 +0100

    third week work

---
+++ week-03/day-5/power/src/power.java
@@ -0,0 +1,23 @@
+public class power {
+// Given base and n that are both 1 or more, compute recursively (no loops) the
+// value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+
+    public static int powerN(int base, int n) {
+
+        if (n == 1) {
+            return base;
+        }
+
+        return powerN(base, n - 1) * base;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int k = powerN(3, 2);
+        System.out.println(k);
+    }
+
+}
