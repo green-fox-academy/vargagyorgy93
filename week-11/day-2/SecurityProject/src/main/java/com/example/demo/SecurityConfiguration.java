1f064a86bb0f99400c980171092bc661859f09e3
vargagyorgy93 <vargagyorgy93@gmail.com>
2021-02-17 13:54:53 +0100

    security with database

--- week-11/day-2/SecurityProject/src/main/java/com/example/demo/SecurityConfiguration.java
+++ week-11/day-2/SecurityProject/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,20 +16,24 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+//    @Autowired
+//    DataSource dataSource;
+
     @Autowired
-    DataSource dataSource;
+    UserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-//                .usersByUsernameQuery("select username, password, enabled"
-//                        + "from users"
-//                        + "where username = ?")
-//                .authoritiesByUsernameQuery("select username, authority"
-//                        + "from authorities"
-//                        + "where username = ?");
-        ;
+        auth.userDetailsService(userDetailsService);
+//        auth.jdbcAuthentication()
+//                .dataSource(dataSource);
+////                .usersByUsernameQuery("select username, password, enabled"
+////                        + "from users"
+////                        + "where username = ?")
+////                .authoritiesByUsernameQuery("select username, authority"
+////                        + "from authorities"
+////                        + "where username = ?");
+
     }
 
     @Override
---
+++ week-11/day-2/SecurityProject/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  int id;
+    private String userName;
+    private String password;
+    private boolean active;
+    private String roles;
+
+
+}
